b73528311c7eb1338259acb6371f41d4046abe1f
Dogth <dogth@kitteth.com>
2022-11-14 02:51:12 +0000

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component, useState } from 'react';
-import axios from 'axios';
 
 import Forecast from './components/Forecast';
 
 export default function App() {
-  // -- Hooks --
+
 
   return (
     <Forecast />
   );
 }
-
-//<div className="App">
-//<button onClick={() => {Endpoint(10,10) }}>Sus</button>
-//</div>
-
-
-// https://api.weather.yandex.ru/v1/informers? 
-// 87978085-2aaa-42fe-a69b-1dbd5aef136b
-/*
-<div>
-
-    <div className='Wrapper'>
-      <div>
-      <button onClick={async () => {await onLoadForecast()}}></button>
-        Susy
-      </div>
-      <div>
-        {Data}
-      </div>
-      <div>
-    ABOUTs
-    <Tax />
-    </div>
-    </div>
-
-  </div>
-*/
